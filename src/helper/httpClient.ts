9ae80102733fce69a48947eda14de6b834d2af57
v-nrenold_microsoft <v-nrenold_microsoft@microsoft.com>
2025-05-03 15:10:13 +0530

    setup http client

--- src/helper/httpClient.ts
+++ src/helper/httpClient.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 import { getBearerToken } from "./MSALAuth";
 import { loginRequest } from "./authConfig";
 import { AlertProps } from "../component/Admin/Admin";
@@ -7,141 +7,72 @@ class httpClient {
   static baseUrl = import.meta.env.VITE_BaseURL;
   static graphApiUrl = "https://graph.microsoft.com/v1.0/";
 
-  public static async getAsync<T>(
+  public static async getAsync<T extends axiosTypes<T>>(
     url: string,
     scopes: string[] = loginRequest.scopes,
     setAlert?: React.Dispatch<React.SetStateAction<AlertProps>>,
     setIsLoading?: React.Dispatch<React.SetStateAction<boolean>>,
+    isGraph = false,
   ) {
     let header = await this.getAuthHeader(scopes);
     if (!header) {
       return;
     }
+    let response = axios.get(
+      `${isGraph ? httpClient.baseUrl : this.graphApiUrl}${url}`,
+      header,
+    );
     if (setIsLoading) {
       setIsLoading(true);
     }
-    let response = await axios.get(`${httpClient.baseUrl}${url}`, header);
+    let result = await httpClient.handleHttpResponse<T>(response, setAlert);
     if (setIsLoading) {
       setIsLoading(false);
     }
-    return httpClient.handleHttpResponse<T>(response, setAlert);
+    return result;
   }
 
-  public static async postAsync<T>(
+  public static async postAsync<T extends axiosTypes<T>>(
     url: string,
     request: any,
     scopes: string[] = loginRequest.scopes,
     setAlert: React.Dispatch<React.SetStateAction<AlertProps>>,
     setIsLoading: React.Dispatch<React.SetStateAction<boolean>>,
+    isGraph = false,
   ) {
     let header = await this.getAuthHeader(scopes);
     if (!header) {
       return;
     }
-    if (setIsLoading) {
-      setIsLoading(true);
-    }
-    let response = await axios.post(
-      `${httpClient.baseUrl}${url}`,
+    let response = axios.post(
+      `${isGraph ? httpClient.baseUrl : this.graphApiUrl}${url}`,
       request,
       header,
     );
     if (setIsLoading) {
-      setIsLoading(false);
-    }
-    return httpClient.handleHttpResponse<T>(response, setAlert);
-  }
-  public static async getGraphAsync<T>(
-    url: string,
-    scopes: string[] = loginRequest.scopes,
-    setAlert: React.Dispatch<React.SetStateAction<AlertProps>>,
-    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>,
-  ) {
-    let header = await this.getAuthHeader(scopes);
-    if (!header) {
-      return;
-    }
-    if (setIsLoading) {
       setIsLoading(true);
     }
-    let response = await axios.get(`${httpClient.graphApiUrl}${url}`, header);
+    let result = await httpClient.handleHttpResponse<T>(response, setAlert);
     if (setIsLoading) {
       setIsLoading(false);
     }
-    return httpClient.handleHttpResponse<T>(response, setAlert);
+    return result;
   }
 
-  public static async postGraphAsync<T>(
-    url: string,
-    request: any,
-    scopes: string[] = loginRequest.scopes,
-    setAlert: React.Dispatch<React.SetStateAction<AlertProps>>,
-    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>,
-  ) {
-    let header = await this.getAuthHeader(scopes);
-    if (!header) {
-      return;
-    }
-    if (setIsLoading) {
-      setIsLoading(true);
-    }
-    let response = await axios.post(
-      `${httpClient.graphApiUrl}${url}`,
-      request,
-      header,
-    );
-    if (setIsLoading) {
-      setIsLoading(false);
-    }
-    return httpClient.handleHttpResponse<T>(response, setAlert);
-  }
-
-  public static handleHttpResponse<T>(
-    response: AxiosResponse,
+  public static async handleHttpResponse<T extends axiosTypes<T>>(
+    result: Promise<AxiosResponse<any, any>>,
     setAlert?: React.Dispatch<React.SetStateAction<AlertProps>>,
-  ) {
-    if (response.status >= 200 && response.status < 400) {
-      return response.data as T;
-    } else if (response.status == 400) {
-      if (setAlert) {
-        let alert: AlertProps = {
-          message:
-            response.data.message.error ||
-            response.data.message ||
-            response.data,
-          severity: "error",
-          show: true,
-        };
-        setAlert(alert);
-        alert = {
-          message: "",
-          severity: "info",
-          show: false,
-        };
-        setTimeout(() => {
-          setAlert(alert);
-        }, 5000);
-      }
-    } else {
-      console.log("Exception:", response.data);
-      if (setAlert) {
-        let alert: AlertProps = {
-          message: response.data,
-          severity: "error",
-          show: true,
-        };
-        setAlert(alert);
-        alert = {
-          message: "",
-          severity: "info",
-          show: false,
-        };
-        setTimeout(() => {
-          setAlert(alert);
-        }, 5000);
-      }
-    }
-    return;
+  ): Promise<T | undefined> {
+    let response: T | undefined = await result
+      .then((response) => {
+        return response.data.value || (response.data as T);
+      })
+      .catch((err: AxiosError<T>) => {
+        let response = err.response?.data;
+        this.setAlerts(response, setAlert);
+        return;
+      });
+    return response;
   }
 
   static async getAuthHeader(
@@ -153,11 +84,50 @@ class httpClient {
     }
     return { headers: { Authorization: `Bearer ${bearerToken}` } };
   }
+
+  static async setAlerts<T extends axiosTypes<T>>(
+    response?: T,
+    setAlert?: React.Dispatch<React.SetStateAction<AlertProps>>,
+  ) {
+    let graphMessage;
+    if (response?.error?.message && response?.error?.message.length > 0) {
+      graphMessage = response?.error?.message[0];
+    }
+    if (setAlert) {
+      let alert: AlertProps = {
+        message:
+          graphMessage ||
+          response?.message ||
+          (response ? response.toString() : "Error occured"),
+        severity: "error",
+        show: true,
+      };
+      setAlert(alert);
+      alert = {
+        message: "",
+        severity: "info",
+        show: false,
+      };
+      setTimeout(() => {
+        setAlert(alert);
+      }, 5000);
+    }
+  }
 }
 
 export interface IHttpClient {
   getAsync: (url: string) => any;
   postAsync: (url: string, request: any) => Promise<any>;
+}
+
+export interface message {
+  message: string[];
+}
+
+export interface axiosTypes<T> {
+  value: T;
+  message: string;
+  error: message;
 }
 
 export default httpClient;
