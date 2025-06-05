78387300ae05c07414557397f0fbc8e2841dd445
naveenrenold <naveenrenold@gmail.com>
2025-06-05 09:43:08 +0530

    pushing uqery updates

--- src/temp_sql/sql1.sql
+++ src/temp_sql/sql1.sql
@@ -1,10 +1,40 @@
-Select * from ActivityType
+select * from form
 
-insert into ActivityType values('CreateForm')
-insert into ActivityType values('EditForm')
-insert into ActivityType values('CompleteForm')
-insert into ActivityType values('AddAttachment')
-insert into ActivityType values('DeleteAttachment')
+insert into form values ('1', '2','3','Test2','Test2 Address', 'NaveenRenold@navigatorxdd.onmicrosoft.com', getdate(),'NaveenRenold@navigatorxdd.onmicrosoft.com', getdate(), '2', '7358639622', '7358639621')
+
+update form set StatusId = '2'
+select * from Attachment
+--100089
 
 
-select * from activity
+insert into Attachment values ('100090', 'file1.pdf', 'file1.pdf','NaveenRenold@navigatorxdd.onmicrosoft.com', getdate(),'application/pdf', 2000)
+insert into Attachment values ('100090', 'file2.pdf', 'file2.pdf','NaveenRenold@navigatorxdd.onmicrosoft.com', getdate(),'application/pdf', 3000)
+
+select * from activity;
+
+select * from activityType;
+
+insert into activity VALUES(100090, 1, '', 'NaveenRenold@navigatorxdd.onmicrosoft.com', getdate(), 'Form', '', '')
+
+insert into activity VALUES(100090, 2, '', 'NaveenRenold@navigatorxdd.onmicrosoft.com', getdate(), 'CustomerName', 'Test1', 'Test2')
+
+insert into activity VALUES(100090, 3, '', 'NaveenRenold@navigatorxdd.onmicrosoft.com', getdate(), 'Form', '', '')
+
+insert into activity VALUES(100090, 4, '', 'NaveenRenold@navigatorxdd.onmicrosoft.com', getdate(), 'FileName', '', 'file1.pdf, file2.pdf,file3.pdf')
+
+insert into activity VALUES(100090, 5, '', 'NaveenRenold@navigatorxdd.onmicrosoft.com', getdate(), 'FileName', 'file1.pdf', '')
+
+
+DECLARE @Id int = 100090,
+@ActivityTypeId int = 1,
+@Comments nvarchar(100) = 'Test Comments',
+@CreatedBy nvarchar(100) = 'NaveenRenold@navigatorxdd.onmicrosoft.com',
+@CreatedOn datetime = getdate(),
+@Field nvarchar(100) = 'TestField',
+@OldValue nvarchar(100) = 'OldValue',
+@NewValue nvarchar(100) = 'NewValue';
+
+INSERT INTO activity (Id, ActivityTypeId, Comments, CreatedBy, CreatedOn, Field, OldValue, NewValue)
+VALUES (@Id, @ActivityTypeId, @Comments, @CreatedBy, @CreatedOn, @Field, @OldValue, @NewValue);
+
+
